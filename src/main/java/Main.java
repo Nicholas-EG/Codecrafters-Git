4a1bd434c602afc8e48629ff574e5ff3e70e1933
Nicholas <141982119+Nicholas-EG@users.noreply.github.com>
2025-05-14 15:59:56 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
@@ -20,6 +22,7 @@ public class Main {
         case "init" -> init();
         case "cat-file" -> catFile(args);
         case "hash-object" -> hashObject(args);
+        case "ls-tree" -> lsTree(args);
         default -> System.out.println("Unknown command: " + command);
       }
     } catch (IOException e) {
@@ -77,6 +80,54 @@ public class Main {
       }
     }
     System.out.println(messageSHA);
+  }
+
+  public static void lsTree(String[] args) throws IOException {
+    boolean nameOnly = args[1].equals("--name-only");
+    String filename = nameOnly ? args[2] : args[1];
+    Path filepath = Paths.get(String.format("%s/.git/objects/%s/%s", System.getProperty("user.dir"),
+        filename.substring(0, 2), filename.substring(2)));
+    byte[] file = Files.readAllBytes(filepath);
+    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+      decompress(file, os);
+      formatTree(os.toByteArray(), nameOnly);
+    }
+  }
+
+  public static void formatTree(byte[] tree, boolean nameOnly) {
+    String[][] terms = getObjectsFromTree(tree);
+    for (String[] term : terms) {
+      if (nameOnly)
+        System.out.println(String.format("%s", term[1]));
+      else {
+        System.out.println(
+            String.format("%s %s %s\t%s", term[0], (term[0].equals("40000")) ? "tree" : "blob", term[2], term[1]));
+      }
+    }
+  }
+
+  public static String[][] getObjectsFromTree(byte[] tree) {
+    ArrayList<String[]> result = new ArrayList<>();
+    int start = indexOf(tree, 0, (byte) 0) + 1;
+    for (int i = start; i < tree.length; i = indexOf(tree, i, (byte) 0) + 21) {
+      String[] term = new String[3];
+      int modeDelineator = indexOf(tree, i, (byte) ' ');
+      int shaDelineator = indexOf(tree, i, (byte) 0);
+      term[0] = bytesToString(Arrays.copyOfRange(tree, i, modeDelineator));
+      term[1] = bytesToString(Arrays.copyOfRange(tree, modeDelineator, shaDelineator)).substring(1);
+      term[2] = bytesToHex(Arrays.copyOfRange(tree, shaDelineator, shaDelineator + 21)).substring(2);
+      result.add(term);
+    }
+    result.sort((a, b) -> a[1].compareTo(b[1]));
+    return result.toArray(new String[0][0]);
+  }
+
+  public static int indexOf(byte[] arr, int start, byte term) {
+    for (int i = start; i < arr.length; i++) {
+      if (arr[i] == term)
+        return i;
+    }
+    return arr.length;
   }
 
   public static String getBlobHash(String message) {
